b33342b26705e184379d679d4994f48f0f7c2fd0
Jacob10383 <36573259+Jacob10383@users.noreply.github.com>
2022-06-06 17:49:57 -0700

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,94 +1,182 @@
 // bst.cpp
 // CS24 Final Exam
 // Implements class bst
-// YOUR NAME(S), DATE
+// JT Siebold 06/06
 
 #include "bst.h"
 
 #include <iostream>
 using std::cout;
 
-
-
-bst::bst(vector<int>&v){
-    root=nullptr;
-this->insert(3);
+bst::bst(vector<int> &v)
+{
+    root = nullptr;
+    for (int i = 0; i < v.size(); i++)
+    {
+        insert(v.at(i));
+    }
 }
 
-bst::~bst(){
-
+bst::~bst()
+{
+    clear(root);
 }
 
+void bst::clear(bstNode *n)
+{
+    if (n)
+    {
+        clear(n->right);
+        clear(n->left);
+        delete n;
+    }
+}
 
+bool bst::insert(int value)
+{
 
-bool bst::insert(int value) {
-
-    if (root == nullptr){
+    if (root == nullptr)
+    {
         root = new bstNode(value);
         return true;
-    }else{
+    }
+    else
+    {
         return insert(value, root);
     }
 
-    return false; 
-
+    return false;
 }
-
 
 // recursive helper for insert (assumes n is never 0)
 
-bool bst::insert(int value, bstNode *n) {
+bool bst::insert(int value, bstNode *n)
+{
 
-    if(value < n->info){
-        if(n->left == nullptr){
+    if (value < n->info)
+    {
+        if (n->left == nullptr)
+        {
             n->left = new bstNode(value);
             n->left->parent = n;
             return true;
-        } else {
+        }
+        else
+        {
             return insert(value, n->left);
         }
-    } else if (value > n->info) {
-        if (n->right == nullptr){
+    }
+    else if (value > n->info)
+    {
+        if (n->right == nullptr)
+        {
             n->right = new bstNode(value);
             n->right->parent = n;
             return true;
-        } else {
+        }
+        else
+        {
             return insert(value, n->right);
         }
-    } else {
+    }
+    else
+    {
         return false;
     }
-
 }
 
-
-void bst::deleteSubtree(int key){
-
-}
-int bst::countLeaves(bstNode *n) const{
-    return 0;
-}
-int bst::countParentsWithTwoChildren(bstNode *n) const{
-    return 0;
-}
-int bst::height(bstNode *n) const{
-    return 0;
-}
-void bst::outputPreOrder(bstNode *n, vector<int>& output) const{
-    return;
-}
-void bst::outputInOrder(bstNode *n, vector<int>& output) const{
-   return;
-}
-
-typename bst::bstNode* bst::getNodeFor(int value, bstNode* n) const{
-    while (n != nullptr) {
-        if (value == n->info) {
-            return n; 
+void bst::deleteSubtree(int key)
+{
+    bstNode *n = getNodeFor(key, root);
+    if (n)
+    {
+        n->right = n->left = nullptr;
+        if (n->parent)
+        {
+            if (n->parent->right)
+                if (n->parent->right->info == n->info)
+                    n->parent->right = nullptr;
+            if (n->parent->left)
+                if (n->parent->left->info == n->info)
+                    n->parent->left = nullptr;
         }
-        else if (value < n->info) {
+    }
+}
+
+int bst::countLeaves(bstNode *n) const
+{
+    if (!n)
+        return 0;
+    if ((n->right == nullptr) && (n->left == nullptr))
+        return 1;
+    else
+        return countLeaves(n->right) + countLeaves(n->left);
+}
+
+int bst::countParentsWithTwoChildren(bstNode *n) const
+{
+    if (!n)
+        return 0;
+    if ((n->right) && (n->left))
+        return 1;
+    else
+        return countLeaves(n->right) + countLeaves(n->left);
+}
+
+int bst::height(bstNode *n) const
+{
+    int tall;
+    if (!root)
+        return -1;
+    if (!n)
+        return 0;
+    else
+    {
+        int left = height(n->left);
+        int right = height(n->right);
+        if (left > right)
+            tall = left;
+        else
+            tall = right;
+        return tall + 1;
+    }
+}
+void bst::outputPreOrder(bstNode *n, vector<int> &output) const
+{
+    if (!n)
+    {
+        return;
+    }
+    output.push_back(n->info);
+    outputInOrder(n->left, output);
+    outputInOrder(n->right, output);
+}
+
+void bst::outputInOrder(bstNode *n, vector<int> &output) const
+{
+    if (!n)
+    {
+        return;
+    }
+    outputInOrder(n->left, output);
+    output.push_back(n->info);
+    outputInOrder(n->right, output);
+}
+
+typename bst::bstNode *bst::getNodeFor(int value, bstNode *n) const
+{
+    while (n != nullptr)
+    {
+        if (value == n->info)
+        {
+            return n;
+        }
+        else if (value < n->info)
+        {
             return getNodeFor(value, n->left);
-        } else {
+        }
+        else
+        {
             return getNodeFor(value, n->right);
         }
     }
